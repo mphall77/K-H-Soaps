4d1883f1bed9727d6aa8c631bcf6c6014de65082
Marsha Hall <mphall77@gmail.com>
2021-12-07 09:11:29 -0500

    install sass, change App.css to .scss

--- k-store-frontend/src/App.js
+++ k-store-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // COMPONENTS
